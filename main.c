88587dc64b7152e5af369f6a46a7171d6fc7b2d2
Jim C. Williams <107096799+jcwml@users.noreply.github.com>
2022-08-16 06:03:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -111,12 +111,10 @@ void rainbow_line_printf(const char* text)
 //https://www.cl.cam.ac.uk/~mgk25/ucs/keysymdef.h
 int key_is_pressed(KeySym ks)
 {
-    Display *dpy = XOpenDisplay(":0");
     char keys_return[32];
-    XQueryKeymap(dpy, keys_return);
+    XQueryKeymap(d, keys_return);
     KeyCode kc2 = XKeysymToKeycode(dpy, ks);
     int isPressed = !!(keys_return[kc2 >> 3] & (1 << (kc2 & 7)));
-    XCloseDisplay(dpy);
     return isPressed;
 }
 
